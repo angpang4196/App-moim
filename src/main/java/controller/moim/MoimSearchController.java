9926dc784d5b70f6f9af1512e7cdf6d8576b7014
angpang4196 <colplayer@naver.com>
2023-04-05 09:43:08 +0900

    feat:기본 페이징 구현

--- src/main/java/controller/moim/MoimSearchController.java
+++ src/main/java/controller/moim/MoimSearchController.java
@@ -1,8 +1,9 @@
 package controller.moim;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,22 +11,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
 import data.Moim;
 import repository.Moims;
 
 @WebServlet("/moim/search")
-public class MoimSearchController extends HttpServlet{
-	
+public class MoimSearchController extends HttpServlet {
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String[] cates =req.getParameterValues("cate");
+
+		String[] cates = req.getParameterValues("cate");
 		// System.out.println(Arrays.toString(cates));
-		
-		
-		List<Moim> list =Moims.findByCate(cates);
+
+		String page = req.getParameter("page");
+		int p;
+		if (req.getParameter("page") == null) {
+			p = 1;
+		} else {
+			p = Integer.parseInt(req.getParameter("page"));
+		}
+
+		SqlSessionFactory factory = (SqlSessionFactory) req.getServletContext().getAttribute("sqlSessionFactory");
+
+		SqlSession sqlSession = factory.openSession();
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("a", (p - 1) * 6 + 1);
+		map.put("b", 6 * p);
+		List<Moim> list = sqlSession.selectList("findSomeByAtoB", map);
+
+		sqlSession.close();
+
+//		List<Moim> list =Moims.findByCate(cates);
 		req.setAttribute("list", list);
-				
-		req.getRequestDispatcher("/WEB-INF/views/moim/search.jsp").forward(req, resp);		
+
+		req.getRequestDispatcher("/WEB-INF/views/moim/search.jsp").forward(req, resp);
 	}
 }
